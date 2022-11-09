92f8a1a94b82a18dfcb733689d27fa6e1b4206f4
JulianBenchimol <jbenchimol1999@gmail.com>
2022-11-08 21:20:58 -0500

    Read me generates with user input

--- index.js
+++ index.js
@@ -1,13 +1,54 @@
-// TODO: Include packages needed for this application
+const inquierer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+inquierer.prompt([
+    {
+        type: 'input',
+        message: 'Please Input Project Title: ',
+        name: 'projectTitle'
+    },
+    {
+        type: 'input',
+        message: 'Please Input Project Description: ',
+        name: 'description'
+    },
+    {
+        type: 'list',
+        message: 'Please Choose a License: ',
+        name: 'licenseType',
+        choices: ['GNU APGLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT']
+    },
+    {
+        type: 'input',
+        message: 'Please Input GitHub Username: ',
+        name: 'githubUsername'
+    },
+    {
+        type: 'input',
+        message: 'Please Input preferred Email: ',
+        name: 'email'
+    },
+    
+])
+.then((answers)=>{
+    let input = Object.values(answers);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    const [projectTitle, description, licenseType, githubUsername, email] = input;
 
-// TODO: Create a function to initialize app
-function init() {}
+    fs.appendFile('readme.md', `<h1>Project title: ${projectTitle}</h1>\n`, (err)=>
+    err ? console.error(err) : console.log('')
+    )
+    fs.appendFile('readme.md', `<h2>Project Description: ${description}</h2>\n`, (err)=>
+    err ? console.error(err) : console.log('')
+    )
+    fs.appendFile('readme.md', `<h4>License Type: ${licenseType}</h4>\n`, (err)=>
+    err ? console.error(err) : console.log('')
+    )
+    fs.appendFile('readme.md', `<h4>GiHub Username: ${githubUsername}</h4>\n`, (err)=>
+    err ? console.error(err) : console.log('')
+    )
+    fs.appendFile('readme.md', `<h4>Email: ${email}</h4>\n`, (err)=>
+    err ? console.error(err) : console.log('Saved!')
+    )
 
-// Function call to initialize app
-init();
+})
